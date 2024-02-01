35513f6a2c44d9699dd0a4ee4e1c435792a5f1b8
dl.integrations.distribution.supplypme@flightcentre.com <dl.integrations.distribution.supplypme@flightcentre.com>
2024-02-01 21:13:57 +1000

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMzYxNg/spectral.js?branch_slug=main&token=4ee44176-37c2-4edc-a71a-6714ed36629a";
+export default { extends: ruleset };
